74e77358dbb5186186b39d4e77e712555d7c88f1
vildashnetwork <vildashnetwork02@gmail.com>
2026-03-09 22:05:27 +0100

    all

--- server.js
+++ server.js
@@ -26,8 +26,13 @@ app.use(express.urlencoded({ extended: false }));
 // CORS
 app.use(cors({
     origin: process.env.NODE_ENV === 'production'
-        ? process.env.FRONTEND_URL
-        : 'http://localhost:8080',
+        ? [
+            process.env.FRONTEND_URL,
+            'http://localhost:8080',
+            'https://www.yourdomain.com',
+            'https://app.yourdomain.com'
+        ].filter(Boolean)
+        : ['http://localhost:8080', 'http://localhost:3000', 'http://localhost:5173'],
     credentials: true,
 }));
 
